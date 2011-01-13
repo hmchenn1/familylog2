226ebd96f39c03de06bce7e6ea2a7be3da987413
Huanmian Chen <hmchenn1@gmail.com>
2011-01-13 02:52:03 -0500

    add vali in kids

--- app/models/kid.rb
+++ app/models/kid.rb
@@ -1,3 +1,6 @@
 class Kid < ActiveRecord::Base
   belongs_to :parent
+  validates :name, :presence => true, :length   => { :maximum => 20 }
+  validates :date, :presence => true, :length   => { :minimum => 4 }
+  validates :performance, :presence => true, :length   => { :maximum=> 200 } 
 end
